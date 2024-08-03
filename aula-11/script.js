7ec2494a4f239b89c677c214cca4cc91d01d395c
julia <mariajuliaamaraldasilva@gmail.com>
2024-08-03 16:42:47 -0300

    javascript

---
+++ aula-11/script.js
@@ -0,0 +1,18 @@
+const num01 = 10;
+const num02 = 20;
+
+if(num01 < num02){
+    console.log(`O maior número é ${num02}`);
+}else{
+    console.log(`O NUM01 é maior`);
+}
+
+
+const valor1 = prompt("Digite um número: ");
+const valor2 = prompt("Digite outro número: ")
+
+if(valor1 > valor2){
+    console.log(true)
+}else{
+    console.log(false)
+}
